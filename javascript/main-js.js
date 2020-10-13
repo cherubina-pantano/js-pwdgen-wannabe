c463507f06af2ba1743c19070af1df815ac05f74
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-13 15:51:12 +0200

    Password generator (insicurissimo!)

--- javascript/main-js.js
+++ javascript/main-js.js
@@ -1,1 +1,31 @@
-console.log('javascript works');
+// 1. Chiedi all'utente il suo nome
+
+var userName = prompt('Qual è il tuo nome?');
+
+ console.log('Nome: ' + userName);
+
+
+// 2. Chiedi all'utente il suo cognome
+
+var userSurname = prompt('Qual è il tuo cognome?');
+
+console.log('Cognome: ' + userSurname);
+
+
+// 3. Chiedi il suo colore preferito
+
+var userFavColor = prompt('Qual è il tuo colore preferito?');
+
+console.log('Colore: ' + userFavColor);
+
+// 4. scrivi sulla pagina: nomecognomecolorepreferito19
+
+var userInfo = userName + userSurname + userFavColor + '19';
+
+console.log(userInfo);
+
+document.getElementById('compila').innerHTML = userInfo;
+
+
+
+alert('Attenzione: password insicurissima!!!');
